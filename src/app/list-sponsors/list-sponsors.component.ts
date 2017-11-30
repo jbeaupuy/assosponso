fdb41d737c280ddde9c723e9bad14bf24ff3f22a
Courbin Francis <lezebre33560@gmail.com>
2017-11-30 11:54:19 +0100

    pas sponso

--- src/app/list-sponsors/list-sponsors.component.ts
+++ src/app/list-sponsors/list-sponsors.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Http } from '@angular/http';
 import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/filter';
+import {APP_CONSTANT} from '../app.constant';
 
 
 @Component({
@@ -12,12 +14,18 @@ export class ListSponsorsComponent implements OnInit {
 
   assos: any;
   sponsos: any;
+  domaines: object = APP_CONSTANT.DOMAINES;
 
   constructor(private http: Http) {}
 
   ngOnInit() {
     this.assos = this.http.get('assets/data/association.json').map(r => r.json());
     this.sponsos = this.http.get('assets/data/sponsors.json').map(r => r.json());
+  }
+
+  change(event) {
+    this.assos = this.assos.filter(data => data.domaines === event.value);
+    this.sponsos = this.sponsos.filter(data => data.domaines === event.value);
   }
 
 }
